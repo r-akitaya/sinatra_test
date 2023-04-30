54255766121784c45025de4434a0c8451a80dbd5
rina.akitaya <rina.akitaya@takionsd.com>
2023-04-30 19:55:17 +0900

    first commit

---
+++ myapp.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+require 'sinatra/reloader'
+ 
+get '/' do
+    "<h1>Hello World!</h1><p>asdfghjk</p><a href='/test'>test</a>"
+end
+
+# Duplicate comment
+
+get '/test' do
+    "<h2>Hello Worldtest!</h2><p>asdfghjk</p>"
+end
